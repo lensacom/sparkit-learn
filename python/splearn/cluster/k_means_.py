161df0bba3823b841a132f489571c95398cdebad
fulibacsi <andras.fulop@replise.com>
2014-10-27 14:55:17 +0100

    Improved avg k-means method. Added Distributed linear model mixin.

--- python/splearn/cluster/k_means_.py
+++ python/splearn/cluster/k_means_.py
@@ -19,10 +19,9 @@ class SparkKMeans(KMeans):
 
     def _fit_avg(self, X, y=None):
         models = X.map(
-            lambda X: super(SparkKMeans, self).fit(X))
-        model_centers = models.map(lambda x: x.cluster_centers_).collect()
-        avg = np.mean(model_centers, axis=0)
-        avgmodel = models.collect()[0]
+            lambda X: super(SparkKMeans, self).fit(X)).collect()
+        avg = np.mean([model.cluster_centers_ for model in models], axis=0)
+        avgmodel = models[0]
         avgmodel.cluster_centers_ = avg
         self.__dict__.update(avgmodel.__dict__)
         return self
---
+++ python/splearn/linear_model/distributed.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+"""Parallel Linear Model training with partial_fit and averaging"""
+
+class DistributedTrainMixin(object):
+    def _dist_train(self, iterator, model, classes):
+        for X, y in iterator:
+            model.partial_fit(X, y, classes=classes)
+        yield model, 1
+
+
+    def _model_sum(self, m_1, m_2):
+        model_1, count_1 = m_1
+        model_2, count_2 = m_2
+        model_1.coef_ += model_2.coef_
+        model_1.intercept_ += model_2.intercept_
+        return model_1, count_1 + count_2
+
+
+    def parallel_train(self, model, data, classes=None, n_iter=10):
+        for i in range(n_iter):
+            models = data.mapPartitions(lambda x: self._dist_train(x, model, classes))
+            model, count = models.reduce(self._model_sum)
+            model.coef_ /= count
+            model.intercept_ /= count
+        return model
