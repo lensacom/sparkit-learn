04e243de404f744c9ccc49d06f3d6ae6f9db227f
Krisztian Szucs <szucs.krisztian@gmail.com>
2014-10-29 12:18:40 +0100

    Added SparkLinearRegression with a basic test

--- python/splearn/linear_model/base.py
+++ python/splearn/linear_model/base.py
@@ -1,14 +1,55 @@
 # encoding: utf-8
 
+from sklearn.base import copy
 from sklearn.linear_model.base import LinearRegression
-from distributed import DistributedTrainMixin
 
-class SparkLinearRegression (LinearRegression, DistributedTrainMixin):
+from ..rdd import ArrayRDD
 
-    def _fit(self, X, y, classes):
-        return super(SparkLinearRegression, self).fit(X, y)
 
-    def fit(self, Z, n_iter=10, classes=None):
-        avg = super(SparkLinearRegression, self).parallel_train(self, Z, classes, n_iter)
+class SparkLinearModelMixin(object):
+
+    def __add__(self, other):
+        """
+        Add method for Linear models with coef and intercept attributes.
+
+        Parameters
+        ----------
+        other : fitted sklearn linear model
+            Model to add.
+
+        Returns
+        -------
+        model : Linear model
+            Model with updated coefficients.
+        """
+        model = copy.deepcopy(self)
+        model.coef_ += other.coef_
+        model.intercept_ += other.intercept_
+        return model
+
+    def __radd__(self, other):
+        return self if other == 0 else self.__add__(other)
+
+    def __div__(self, other):
+        self.coef_ /= other
+        self.intercept_ /= other
+        return self
+
+    def _spark_fit(self, cls, Z, *args, **kwargs):
+        mapper = lambda (X, y): super(cls, self).fit(X, y, *args, **kwargs)
+        models = Z.map(mapper)
+        avg = models.sum() / models.count()
         self.__dict__.update(avg.__dict__)
         return self
+
+    def _spark_predict(self, cls, X, *args, **kwargs):
+        return X.map(lambda X: super(cls, self).predict(X, *args, **kwargs))
+
+
+class SparkLinearRegression(LinearRegression, SparkLinearModelMixin):
+
+    def fit(self, Z):
+        return self._spark_fit(SparkLinearRegression, Z)
+
+    def predict(self, X):
+        return self._spark_predict(SparkLinearRegression, X)
---
+++ python/test/test_linear_model_base.py
@@ -0,0 +1,53 @@
+import shutil
+import tempfile
+import numpy as np
+import scipy.sparse as sp
+
+from nose.tools import assert_equal
+from nose.tools import assert_true
+from numpy.testing import assert_array_almost_equal
+
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
+
+from common import SplearnTestCase
+from splearn.rdd import ArrayRDD, TupleRDD
+from splearn.linear_model import SparkLinearRegression
+
+
+class LinearModelBaseTestCase(SplearnTestCase):
+
+    def setUp(self):
+        super(LinearModelBaseTestCase, self).setUp()
+        self.outputdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        super(LinearModelBaseTestCase, self).tearDown()
+        shutil.rmtree(self.outputdir)
+
+    def generate_dataset(self, n_targets, n_samples, blocks=None):
+        X, y = make_regression(n_targets=n_targets,
+                               n_samples=n_samples, n_features=20,
+                               n_informative=10, random_state=42)
+        # X = np.abs(X)
+
+        X_rdd = self.sc.parallelize(X)
+        y_rdd = self.sc.parallelize(y)
+
+        Z = TupleRDD(X_rdd.zip(y_rdd), blocks)
+
+        return X, y, Z
+
+
+class TestLinearRegression(LinearModelBaseTestCase):
+
+    def test_same_prediction(self):
+        X, y, Z = self.generate_dataset(1, 100000)
+
+        local = LinearRegression()
+        dist = SparkLinearRegression()
+
+        y_local = local.fit(X, y).predict(X)
+        y_dist = dist.fit(Z).predict(Z.column(0))
+
+        assert_array_almost_equal(y_local, np.concatenate(y_dist.collect()))
