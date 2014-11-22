c46eac343279f58c7305e4456bb4aba4ebae9f44
Krisztian Szucs <szucs.krisztian@gmail.com>
2014-11-22 15:40:36 +0100

    Fixed grid_search test

--- python/test/test_grid_search.py
+++ python/test/test_grid_search.py
@@ -1,19 +1,18 @@
 import shutil
 import tempfile
 import numpy as np
-import scipy.sparse as sp
-
-from numpy.testing import assert_array_almost_equal
 
 from sklearn.datasets import make_classification
-from sklearn.svm import LinearSVC
 
 from common import SplearnTestCase
-from splearn.rdd import ArrayRDD, DictRDD
-from splearn.grid_search  import SparkGridSearchCV
+from splearn.rdd import DictRDD
+from splearn.grid_search import SparkGridSearchCV
 from splearn.naive_bayes import SparkMultinomialNB
 
-from sklearn.cross_validation import KFold
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.grid_search import GridSearchCV
+
+from sklearn.utils.testing import assert_array_almost_equal
 
 
 class GridSearchTestCase(SplearnTestCase):
@@ -41,15 +40,25 @@ class GridSearchTestCase(SplearnTestCase):
 
 class TestGridSearchCV(GridSearchTestCase):
 
-    def test_basic(self):
-        X, y, Z = self.generate_dataset(2, 1000, 100)
+    def test_same_result(self):
+        X, y, Z = self.generate_dataset(2, 30000, 5001)
 
-        estimator = SparkMultinomialNB()
         parameters = {'alpha': [0.1, 1, 10]}
         fit_params = {'classes': np.unique(y)}
+
+        local_estimator = MultinomialNB()
+        local_grid = GridSearchCV(estimator=local_estimator,
+                                  param_grid=parameters)
+
+        estimator = SparkMultinomialNB()
         grid = SparkGridSearchCV(estimator=estimator,
                                  param_grid=parameters,
                                  fit_params=fit_params)
 
-        result = grid.fit(Z)
-        print grid.grid_scores_
+        local_grid.fit(X, y)
+        grid.fit(Z)
+
+        locscores = [r.mean_validation_score for r in local_grid.grid_scores_]
+        scores = [r.mean_validation_score for r in grid.grid_scores_]
+
+        assert_array_almost_equal(locscores, scores, decimal=2)
