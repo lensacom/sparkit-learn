bfed5a3429f428cf1123f2fe2139b8cc5efc8e2e
fulibacsi <andras.fulop@replise.com>
2014-11-24 16:10:59 +0100

    Added module imports to feature_extraction; Added documentation to SparkDictVectorizer.

---
+++ python/splearn/feature_extraction/__init__.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+from .text import SparkCountVectorizer
+from .text import SparkHashingVectorizer
+from .text import SparkTfidfTransformer
+from .dict_vectorizer import SparkDictVectorizer
