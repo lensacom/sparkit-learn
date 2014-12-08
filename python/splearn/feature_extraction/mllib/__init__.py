b1090d38e71172e34c4c58849e6995aa086b467f
Krisztian Szucs <szucs.krisztian@gmail.com>
2014-12-08 18:30:47 +0100

    Renamed mllib based scala dict_vectorizer

---
+++ python/splearn/feature_extraction/mllib/__init__.py
@@ -0,0 +1,4 @@
+# encoding: utf-8
+
+from .dict_vectorizer import MllibDictVectorizer
+
--- python/splearn/feature_extraction/mllib/dict_vectorizer.py
+++ python/splearn/feature_extraction/mllib/dict_vectorizer.py
@@ -3,7 +3,6 @@
 from pyspark.mllib._common import _deserialize_double_vector
 from pyspark.serializers import FramedSerializer
 from pyspark.rdd import RDD
-from pyspark import SparkContext
 
 
 class DoubleVectorSerializer(FramedSerializer):
@@ -15,7 +14,7 @@ class DoubleVectorSerializer(FramedSerializer):
         return _deserialize_double_vector(bytearray(obj))
 
 
-class DictVectorizer(object):
+class MllibDictVectorizer(object):
 
     def __init__(self):
         self._vocab = None
@@ -30,7 +29,7 @@ class DictVectorizer(object):
         sc = rdd.context
         self.api = sc._jvm.PythonDictVectorizerAPI()
         self.api.fit(rdd._jrdd)
-        
+
     def transform(self, rdd):
         sc = rdd.context
         if not self.api:
@@ -43,27 +42,3 @@ class DictVectorizer(object):
         self.api = sc._jvm.PythonDictVectorizerAPI()
         jrdd = self.api.fitTransform(rdd._jrdd)
         return RDD(jrdd, sc, DoubleVectorSerializer())
-
-
-def test_dictvectorizer(sc):
-    test_data = [[('foo', 1), ('bar', 2)], [('foo', 3), ('baz', 1)]]
-    rdd = sc.parallelize(test_data)
-    dv = DictVectorizer()
-    print dv.fit_transform(rdd).collect()
-    print dv.vocab
-
-
-def test_dictvectorizer_2(sc):
-    test_data = [[('foo', 1), ('bar', '2')], [('foo', 3), ('baz', '1')]]
-    rdd = sc.parallelize(test_data)
-    dv = DictVectorizer()
-    dv.fit(rdd)
-    print dv.transform(rdd).collect()
-    print dv.vocab
-
-
-if __name__ == "__main__":
-    sc = SparkContext(appName="lensaNLP test")
-    test_dictvectorizer(sc)
-    test_dictvectorizer_2(sc)
-    sc.stop()
