6860970ee07e6b012e0182d9562a8147a17458d5
Thomas Aynaud <thomas@clustree.com>
2015-11-20 17:33:23 +0100

    Add conversion to scikit for FeatureUnion

--- splearn/pipeline.py
+++ splearn/pipeline.py
@@ -286,6 +286,15 @@ class SparkFeatureUnion(FeatureUnion):
             out.update(super(SparkFeatureUnion, self).get_params(deep=False))
             return out
 
+    def to_scikit(self):
+        sk_transformer_list = []
+        for name, transformer in self.transformer_list:
+            if hasattr(transformer, 'to_scikit'):
+                sk_transformer_list.append((name, transformer.to_scikit()))
+            else:
+                sk_transformer_list.append((name, transformer))
+        return FeatureUnion(sk_transformer_list)
+
 
 def make_sparkunion(*transformers):
     """Construct a FeatureUnion from the given transformers.
